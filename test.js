adb7cf67068ed29259b77032aa038d95b919a091
David Bjorklund <david.bjorklund@gmail.com>
2016-07-18 12:59:38 +0200

    simplify api, add install & postinstall handlers

--- test.js
+++ test.js
@@ -22,14 +22,45 @@ test('packageLifecycle(), preinstall', async t => {
     }
   }));
 
-  let installCalled = false;
-  await packageLifecycle({
-    dir, install: async () => {
-      installCalled = true;
-      t.true(await fs.exists(join(dir, 'preinstall')));
-      t.false(await fs.exists(join(__dirname, 'preinstall')));
-      return Promise.resolve(null);
+  const {preinstall, install, postinstall} = await packageLifecycle({dir});
+
+  await preinstall();
+  await install();
+  await postinstall();
+  t.true(await fs.exists(join(dir, 'preinstall')));
+  t.false(await fs.exists(join(__dirname, 'preinstall')));
+});
+
+test('packageLifecycle(), install', async t => {
+  const {path: dir} = await tmp.dir();
+  await fs.writeFile(join(dir, 'package.json'), JSON.stringify({
+    scripts: {
+      install: 'touch install'
     }
-  });
-  t.true(installCalled);
+  }));
+
+  const {preinstall, install, postinstall} = await packageLifecycle({dir});
+
+  await preinstall();
+  await install();
+  await postinstall();
+  t.true(await fs.exists(join(dir, 'install')));
+  t.false(await fs.exists(join(__dirname, 'install')));
+});
+
+test('packageLifecycle(), postinstall', async t => {
+  const {path: dir} = await tmp.dir();
+  await fs.writeFile(join(dir, 'package.json'), JSON.stringify({
+    scripts: {
+      postinstall: 'touch postinstall'
+    }
+  }));
+
+  const {preinstall, install, postinstall} = await packageLifecycle({dir});
+
+  await preinstall();
+  await install();
+  await postinstall();
+  t.true(await fs.exists(join(dir, 'postinstall')));
+  t.false(await fs.exists(join(__dirname, 'postinstall')));
 });
